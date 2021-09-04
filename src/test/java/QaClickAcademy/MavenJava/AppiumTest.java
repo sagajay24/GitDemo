be32e7007b09d285606ed0c1d9e5ddfb55b9544a
sagar.s.koli <kolisagar@gmail.com>
2021-09-04 18:18:59 +0530

    My first commit

---
+++ src/test/java/QaClickAcademy/MavenJava/AppiumTest.java
@@ -0,0 +1,17 @@
+package QaClickAcademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest
+{
+	@Test
+	 public void NativeAndroid()
+	 {
+		 System.out.println("NativeAndroid");
+	 }
+	@Test
+	 public void IOSApp()
+	 {
+		 System.out.println("IOSApp");
+	 }
+}
